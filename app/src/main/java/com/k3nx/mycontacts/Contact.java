aa7ea6eedccf3de80f228ff3a31860a604369206
Ken Wilcox <wilcoxk@gmail.com>
2015-01-30 10:42:47 -0700

    added Contact, ContactAdapter

---
+++ app/src/main/java/com/k3nx/mycontacts/Contact.java
@@ -0,0 +1,18 @@
+package com.k3nx.mycontacts;
+
+/**
+ * Created by Ken Wilcox on 1/30/2015 10:33 AM.
+ * Simple Contact Class
+ */
+public class Contact {
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+}
--- app/src/main/java/com/k3nx/mycontacts/ContactListActivity.java
+++ app/src/main/java/com/k3nx/mycontacts/ContactListActivity.java
@@ -4,6 +4,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class ContactListActivity extends ActionBarActivity {
@@ -12,8 +19,30 @@ public class ContactListActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+
+        ArrayList<Contact> contacts = new ArrayList<>();
+        Contact contact1 = new Contact();
+        contact1.setName("Ken Wilcox");
+        contacts.add(contact1);
+
+        ListView listView = (ListView)findViewById(R.id.contact_list_view);
+        listView.setAdapter(new ContactAdapter(contacts));
     }
 
+    private class ContactAdapter extends ArrayAdapter<Contact>{
+        ContactAdapter(ArrayList<Contact> contacts) {
+            super(ContactListActivity.this, R.layout.contact_list_row, R.id.contact_row_name, contacts);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+            Contact contact = getItem(position);
+            TextView nameTextView = (TextView)convertView.findViewById(R.id.contact_row_name);
+            nameTextView.setText(contact.getName());
+            return convertView;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
